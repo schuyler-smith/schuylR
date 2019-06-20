3f686bc9968838f70f0d59870d61cabd01ffc3b6
sdsmith1390 <sdsmith@iastate.edu>
2019-06-20 10:29:20 -0500

    add arg classifiers, card sys.data

---
+++ R/schuylR-package.R
@@ -0,0 +1,19 @@
+#' schuylR-package
+#'
+#' schuylR is a conglomeration of functions written to do
+#' various things in R that I find useful to use frequently.
+#'
+#'\url{https://schuyler-smith.github.io/schuylR/}
+#'
+#'
+#' @name schuylR
+#' @author Schuyler Smith \email{schuyler.smith@@iastate.edu}
+#' @docType package
+#' @keywords package
+#' @import data.table
+NA
+
+# OBO_filepath <- '~/Downloads/card-ontology/aro.obo'
+# CARD <- processOBO(OBO_filepath)
+# load('R/sysdata.rda')
+# save(CARD, file = 'R/sysdata.rda')

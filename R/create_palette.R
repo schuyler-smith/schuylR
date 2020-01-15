3ada57f863a6ed500815ec8c2f49e58a7a7282a1
schuyler-smith <sdsmith@iastate.edu>
2020-01-15 16:52:00 -0600

    add color palette

--- R/create_palette.R
+++ R/create_palette.R
@@ -15,11 +15,10 @@
 
 
 create_palette <- function(color_count, colors = 'default'){
-  options(warn = -1)
   mycolors <- c(
-    "#565656", "#E69F00", "#56B4E9", "#009E73", 
+    "#757575", "#E69F00", "#56B4E9", "#009E73", 
     "#F0E442", "#0072B2", "#D55E00", "#CC79A7", 
-    "#9EDA8F", "#757575", "#DE9861", "#A6CBE0",
+    "#9EDA8F", "#DE9861","#565656",  "#A6CBE0",
     "#B275D8", "#82BB47", "#e0503a", "#F5E56C",
     "#949696", "#4989DE", "#E2E2E2", 
     "#F7B04C", "#696bb2")

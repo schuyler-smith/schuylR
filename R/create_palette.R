627d70859c4d164bb806f80c2ff1a6cfd144bb46
Schuyler Smith <sdsmith@iastate.edu>
2021-05-24 19:13:44 -0500

    fix palette for large count, adddynamicness

--- R/create_palette.R
+++ R/create_palette.R
@@ -14,32 +14,25 @@
 #' @export
 #' 
 
-create_palette <- function(color_count, colors = 'default', preview = FALSE){
-  mycolors <- c(
-    "#757575", "#E69F00", "#56B4E9", "#009E73", 
-    "#F0E442", "#0072B2", "#D55E00", "#CC79A7", 
-    "#9EDA8F", "#DE9861","#565656",  "#A6CBE0",
-    "#B275D8", "#82BB47", "#e0503a", "#F5E56C",
-    "#949696", "#4989DE", "#E2E2E2", 
-    "#F7B04C", "#696bb2")
-  #"#A8B1CC"
-  if(any(colors == 'default')){
-    colors <- mycolors
-    if(color_count <= length(colors)){
-      palette <- colors[seq(color_count)]
+create_palette <- function (color_count, colors = "default", preview = FALSE) 
+{
+  if (any(colors == "default")) {
+    colors <- c("#757575", "#E69F00", "#56B4E9", "#009E73", 
+                "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#9EDA8F", 
+                "#DE9861", "#565656", "#A6CBE0", "#B275D8", "#82BB47", 
+                "#e0503a", "#F5E56C", "#949696", "#4989DE", "#E2E2E2", 
+                "#F7B04C", "#696bb2")
+  } else if(any(!(colors %in% colors()))) {
+    if (any(colors %in% rownames(RColorBrewer::brewer.pal.info))) {
+      colors <- RColorBrewer::brewer.pal(min(c(color_count, 
+                        RColorBrewer::brewer.pal.info[rownames(RColorBrewer::brewer.pal.info) == colors, 1])), colors)
     }
-  } else if(any(!(colors %in% colors()))){
-    if(any(colors %in% rownames(RColorBrewer::brewer.pal.info))){
-      palette <- grDevices::colorRampPalette(
-        RColorBrewer::brewer.pal(min(c(color_count, 
-                   RColorBrewer::brewer.pal.info[rownames(RColorBrewer::brewer.pal.info) == colors, 1])), 
-                   colors))(color_count)
-    } else { palette <- grDevices::colorRampPalette(colors)(color_count)}
-  } else { palette <- grDevices::colorRampPalette(colors)(color_count)}
-  if(preview){print(graphics::image(1:length(palette), 1, as.matrix(1:length(palette)), 
-                          col=palette, 
-                          xlab="", ylab = "", 
-                          xaxt = "n", yaxt = "n", 
-                          bty = "n"))}
-  return(palette)
+  }
+  colors <- grDevices::colorRampPalette(colors)(color_count)
+  if (preview) {
+    graphics::image(1:length(colors), 1, as.matrix(1:length(colors)), 
+                          col = colors, xlab = "", ylab = "", xaxt = "n", 
+                          yaxt = "n", bty = "n")
+  }
+  return(colors)
 }

09edc43b2163a2ae1586e85b2730b4f15a99535e
Schuyler Smith <sdsmith@iastate.edu>
2021-11-06 11:49:39 -0500

    update color palette

--- R/create_palette.R
+++ R/create_palette.R
@@ -16,20 +16,25 @@
 
 create_palette <- function (color_count, colors = "default", preview = FALSE) 
 {
-  if (any(colors == "default")) {
+  if (any(colors %in% "default")) {
     colors <- c("#757575", "#E69F00", "#56B4E9", "#009E73", 
                 "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#9EDA8F", 
                 "#DE9861", "#565656", "#A6CBE0", "#B275D8", "#82BB47", 
                 "#e0503a", "#F5E56C", "#949696", "#4989DE", "#E2E2E2", 
                 "#F7B04C", "#696bb2")
-  } else if (any(!(colors %in% colors()))) {
-    if (any(colors %in% rownames(RColorBrewer::brewer.pal.info))) {
-      colors <- RColorBrewer::brewer.pal(min(c(color_count, 
-                                               RColorBrewer::brewer.pal.info[rownames(RColorBrewer::brewer.pal.info) == 
-                                                                               colors, 1])), colors)
-    }
+    if(color_count < length(colors)){colors <- colors[seq(color_count)]}
   }
-  if(length(colors) < color_count){colors <- (grDevices::colorRampPalette(colors))(color_count)}
+  if (any(colors %in% rownames(RColorBrewer::brewer.pal.info))) {
+    colors <- RColorBrewer::brewer.pal(min(c(color_count, 
+                                             RColorBrewer::brewer.pal.info[rownames(RColorBrewer::brewer.pal.info) == 
+                                                                             colors, 1])), colors)
+  }
+  if (any(colors %in% "viridis")) {
+    colors <- c("#ffcf20FF", "#10a53dFF", "#2f9aa0FF", "#3a5e8cFF", "#541352FF")
+  }
+  if(length(colors) != color_count){
+    colors <- (grDevices::colorRampPalette(colors))(color_count)
+  }
   colors <- colors[seq(color_count)]
   if (preview) {
     graphics::image(1:length(colors), 1, as.matrix(1:length(colors)), 
